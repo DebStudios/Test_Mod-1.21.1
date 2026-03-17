d944c4a570a1ae26ed72fd06761f3a38040644ee
DebStudios <kobe.debo.2010@gmail.com>
2026-03-17 23:11:22 +0100

    Initial commit

---
+++ src/main/java/net/debrooo/testmod1/TestMod1.java
@@ -0,0 +1,18 @@
+package net.debrooo.testmod1;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestMod1 implements ModInitializer {
+	public static final String MOD_ID = "test-mod1";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
